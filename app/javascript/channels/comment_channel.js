f7abe5dd381b35696616d5d5bf93c5d0714e7f0e
TAKUYA34 <110417799+TAKUYA34@users.noreply.github.com>
2023-02-16 21:57:57 +0900

    jsを使って情報を受け取る処理

--- app/javascript/channels/comment_channel.js
+++ app/javascript/channels/comment_channel.js
@@ -1,15 +1,19 @@
 import consumer from "./consumer"
 
-consumer.subscriptions.create("CommentChannel", {
-  connected() {
-    // Called when the subscription is ready for use on the server
-  },
+if(location.pathname.match(/\/items\/\d/)){
+  
+  consumer.subscriptions.create("CommentChannel", {
+    connected() {
+      // Called when the subscription is ready for use on the server
+    },
 
-  disconnected() {
-    // Called when the subscription has been terminated by the server
-  },
+    disconnected() {
+      // Called when the subscription has been terminated by the server
+    },
 
-  received(data) {
-    // Called when there's incoming data on the websocket for this channel
-  }
-});
+    received(data) {
+      console.log(data) //追加
+      // Called when there's incoming data on the websocket for this channel
+    }
+  });
+}
